162ce1fc7dcff557462599d25c81ce427f1b0ac9
Abner Santana <FineLine>
2020-06-11 23:30:35 -0300

    update
    Estou aprendendo a criar um projeto corretamente

---
+++ leitura-escrita/leitura-calculo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace leitura_calculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
