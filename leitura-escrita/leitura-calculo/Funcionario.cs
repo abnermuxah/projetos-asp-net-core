09d5efbe19df179422857333fa838de3b9afa729
Abner Santana <FineLine>
2020-06-27 12:54:38 -0300

    commit

---
+++ leitura-escrita/leitura-calculo/Funcionario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leitura_calculo
+{
+    class Funcionario
+    {
+        public String nome;
+        public int salario;
+    }
+}
--- leitura-escrita/leitura-calculo/Program.cs
+++ leitura-escrita/leitura-calculo/Program.cs
@@ -6,7 +6,21 @@ namespace leitura_calculo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Funcionario f1 = new Funcionario();
+            Funcionario f2 = new Funcionario();
+
+           
+            Console.WriteLine("Digite o nome do funcionario 1");
+            f1.nome = Console.ReadLine();
+            Console.WriteLine("Digite o salario do funcionario 1");
+            f1.salario = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o nome do funcionario 2");
+            f2.nome = Console.ReadLine();
+            Console.WriteLine("Digite o salario do funcionario 2");
+            f2.salario = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("A media salariao eh " + (f1.salario + f2.salario)/ 2 );
         }
     }
 }
