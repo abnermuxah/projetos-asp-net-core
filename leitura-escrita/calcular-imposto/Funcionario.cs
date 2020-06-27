2b1dd9afed85199367a0b08c469ea5551e4be3d3
Abner Santana <FineLine>
2020-06-27 20:40:02 -0300

    imposto
    acrecsentei a tarefa pra aumentar o salario baseado em uma porcentagem

--- leitura-escrita/calcular-imposto/Funcionario.cs
+++ leitura-escrita/calcular-imposto/Funcionario.cs
@@ -6,5 +6,19 @@ namespace calcular_imposto
 {
     class Funcionario
     {
+        public String Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido(){
+            return SalarioBruto - Imposto;
+        }
+        public void AumentarSalario(double Porcentagem) {
+            SalarioBruto = SalarioBruto + SalarioBruto * (Porcentagem / 100);
+        }
+
+        public override string ToString() {
+            return Nome + " Salario R$: " + SalarioLiquido();
+        }
     }
 }
--- leitura-escrita/calcular-imposto/Program.cs
+++ leitura-escrita/calcular-imposto/Program.cs
@@ -6,7 +6,23 @@ namespace calcular_imposto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Funcionario func = new Funcionario();
+            
+            Console.WriteLine("Digite o nome do funcionario: ");
+            func.Nome = Console.ReadLine();
+            Console.WriteLine("Digite o salario bruto do funcionario: ");
+            func.SalarioBruto = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o imposto a ser cobrado em reais");
+            func.Imposto = double.Parse(Console.ReadLine());
+           
+            Console.WriteLine("Resultado: " + func);
+
+            Console.WriteLine(" Quer aumentar o salario em quantos por cento ?");
+            double porcent = double.Parse(Console.ReadLine());
+            func.AumentarSalario(porcent);
+
+            Console.WriteLine("Resultado atualizado: " + func);
+
         }
     }
 }
