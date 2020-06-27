593ee6cbdb5e54d6d88d0a6c94e04f2dccec1df4
Abner Santana <FineLine>
2020-06-27 18:54:30 -0300

    calculo do retangulo

---
+++ leitura-escrita/geometria-retangulo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace geometria_retangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret = new Retangulo();
+            Console.WriteLine("Digite a altura e largura do retangulo");
+            ret.Altura = double.Parse(Console.ReadLine());
+            ret.Largura = double.Parse(Console.ReadLine());
+            Console.WriteLine("Area:" + ret.Area());
+            Console.WriteLine("Perimetro:" + ret.Perimetro());
+            Console.WriteLine("Diagonal:" + ret.Diagonal());
+        }
+    }
+}
---
+++ leitura-escrita/geometria-retangulo/Retangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace geometria_retangulo
+{
+    class Retangulo
+    {
+        public double Altura;
+        public double Largura;
+
+        public double Area(){
+            return Altura * Largura;
+        }
+        public double Perimetro(){
+            return 2* (Altura + Largura);
+        }
+
+        public double Diagonal(){
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+
+    }
+}
