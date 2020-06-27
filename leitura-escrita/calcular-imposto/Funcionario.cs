66883f73770a2d08e290fadcd916d828c7e7af95
Abner Santana <FineLine>
2020-06-27 19:13:21 -0300

    new

---
+++ leitura-escrita/calcular-imposto/Funcionario.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calcular_imposto
+{
+    class Funcionario
+    {
+    }
+}
---
+++ leitura-escrita/calcular-imposto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace calcular_imposto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
